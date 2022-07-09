6433ccf74d5d05edf264bf1477a4b5e7da066b3c
Naohiro Ishihara <nishi@NaohironoMacBook-Pro.local>
2022-07-09 14:32:14 +0900

    port指定修正

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port = process.env.PORT || 8080;
+const port = Number(process.env.PORT) || 8080;
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
